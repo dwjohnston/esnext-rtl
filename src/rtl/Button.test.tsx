55c06eaf1597572177c248cda89d39aae898d96a
David Johnston <djohnston@whispir.com>
2020-12-10 17:29:24 +1100

    Repro the issue i have

--- src/rtl/Button.test.tsx
+++ src/rtl/Button.test.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+import {foo} from "foo";
+
 it('should call the click handler', () => {
     const mockClickHandler = jest.fn();
-    render(
-        <button
+    render(<button
             data-testid="button"
             onClick={mockClickHandler}
-        />
+        >hello</button>
     );
 
-    userEvent.click(screen.getByRole('button'));
+    console.log(foo);
+    userEvent.click(screen.getByText('aaaa'));
     // OR, as a recommended escape hatch: 
     // userEvent.click(screen.getByTestId('button'));
     expect(mockClickHandler).toHaveBeenCalled();
